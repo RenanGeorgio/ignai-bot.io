c0b317851b9d0435dda472642dec41b0c28321ff
RenanGeorgio <regeo9@hotmail.com>
2024-05-16 15:05:53 -0300

    try

--- apps/builder/src/components/EditableEmojiOrImageIcon.tsx
+++ apps/builder/src/components/EditableEmojiOrImageIcon.tsx
@@ -93,7 +93,7 @@ export const EditDialogEmojiOrImageIcon = ({
   onConfirm
 }: DialogProps) => {
   const { t } = useTranslate()
-  const cancelRef: RefObject<any> | MutableRefObject<undefined> = useRef(null)
+  const cancelRef: RefObject<unknown> | MutableRefObject<undefined> = useRef(null)
 
   const onChangeClick = async (icon: string) => {
     try {
