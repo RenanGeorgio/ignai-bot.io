6b22e016a3748e324bbba5628b0fd97ee07d4ddc
RenanGeorgio <regeo9@hotmail.com>
2024-05-22 05:41:27 -0300

    try

--- apps/builder/create-theme.d.ts
+++ apps/builder/create-theme.d.ts
@@ -5,6 +5,18 @@ type CustomProps = {
 }
 
 declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xs?: true
+    sm?: true
+    md?: true
+    lg?: true
+    xl?: true
+    mobile?: true
+    tablet?: true
+    laptop?: true
+    desktop?: true
+  }
+
   interface Theme {
     status: {
       danger: React.CSSProperties["color"] | string
@@ -13,6 +25,7 @@ declare module '@mui/material/styles' {
 
   interface Palette {
     neutral?: Palette["primary"]
+    tertiary?: Palette["primary"]
     gradient?: PaletteColor
     transparent?: PaletteColor
     secondary?: {
@@ -22,15 +35,18 @@ declare module '@mui/material/styles' {
   }
   interface PaletteOptions {
     neutral?: PaletteOptions["primary"]
+    tertiary?: PaletteOptions["primary"]
   }
 
   interface PaletteColor {
     darker?: string
     transparent?: string
     gradient?: PaletteColor
+    lighter?: string
   }
   interface SimplePaletteColorOptions {
     darker?: string
+    lighter?: string
   }
   interface PaletteColorOptions {
     main?: string
@@ -124,23 +140,122 @@ declare module '@mui/material/styles' {
 
 declare module '@mui/material/styles/overrides' {
   interface ComponentNameToClassKey {
-    MUIDataTable?: string | CustomProps
-    MUIDataTableBody?: string | CustomProps
-    MUIDataTableBodyCell?: string | CustomProps
-    MUIDataTableBodyRow?: string | CustomProps
-    MUIDataTableFilter?: string | CustomProps
-    MUIDataTableFilterList?: string | CustomProps
-    MUIDataTableFooter?: string | CustomProps
-    MUIDataTableHead?: string | CustomProps
-    MUIDataTableHeadCell?: string | CustomProps
-    MUIDataTableHeadRow?: string | CustomProps
-    MUIDataTableJumpToPage?: string | CustomProps
-    MUIDataTablePagination?: string | CustomProps
-    MUIDataTableResize?: string | CustomProps
-    MUIDataTableSearch?: string | CustomProps
-    MUIDataTableSelectCell?: string | CustomProps
-    MUIDataTableToolbar?: string | CustomProps
-    MUIDataTableToolbarSelect?: string | CustomProps
-    MUIDataTableViewCol?: string | CustomProps
+    MUIDataTable: 'root' | 'caption' | 'liveAnnounce' | 'paper' | 'responsiveScroll' | 'tableRoot'
+
+    MUIDataTableBody: 'root' | 'emptyTitle' | 'lastSimpleCell' | 'lastStackedCell'
+
+    MUIDataTableBodyCell:
+        | 'root'
+        | 'cellHide'
+        | 'cellStackedSmall'
+        | 'responsiveStackedSmall'
+        | 'responsiveStackedSmallParent'
+        | 'simpleCell'
+        | 'simpleHeader'
+        | 'stackedCommon'
+        | 'stackedCommonAlways'
+        | 'stackedHeader'
+        | 'stackedParent'
+        | 'stackedParentAlways'
+
+    MUIDataTableBodyRow: 'root' | 'hoverCursor' | 'responsiveSimple' | 'responsiveStacked'
+
+    MUIDataTableFilter:
+        | 'root'
+        | 'checkbox'
+        | 'checkboxFormControl'
+        | 'checkboxFormControlLabel'
+        | 'checkboxFormGroup'
+        | 'checkboxIcon'
+        | 'checkboxListTitle'
+        | 'checked'
+        | 'filtersSelected'
+        | 'gridListTile'
+        | 'header'
+        | 'noMargin'
+        | 'reset'
+        | 'resetLink'
+        | 'title'
+
+    MUIDataTableFilterList: 'root' | 'chip'
+
+    MUIDataTableFooter: 'root'
+
+    MUIDataTableHead: 'main' | 'responsiveSimple' | 'responsiveStacked' | 'responsiveStackedAlways'
+
+    MUIDataTableHeadCell:
+        | 'root'
+        | 'contentWrapper'
+        | 'data'
+        | 'dragCursor'
+        | 'fixedHeader'
+        | 'hintIconAlone'
+        | 'hintIconWithSortIcon'
+        | 'mypopper'
+        | 'sortAction'
+        | 'sortActive'
+        | 'sortLabelRoot'
+        | 'toolButton'
+        | 'tooltip'
+
+    MUIDataTableHeadRow: 'root'
+
+    MUIDataTableJumpToPage: 'root' | 'caption' | 'input' | 'select' | 'selectIcon' | 'selectRoot'
+
+    MUIDataTablePagination:
+        | 'root'
+        | '@media screen and (max-width: 400px)'
+        | 'navContainer'
+        | 'selectRoot'
+        | 'tableCellContainer'
+        | 'toolbar'
+
+    MUIDataTableResize: 'root' | 'resizer'
+
+    MUIDataTableSearch: 'clearIcon' | 'main' | 'searchIcon' | 'searchText'
+
+    MUIDataTableSelectCell:
+        | 'root'
+        | 'checkboxRoot'
+        | 'checked'
+        | 'disabled'
+        | 'expandDisabled'
+        | 'expanded'
+        | 'fixedHeader'
+        | 'fixedLeft'
+        | 'headerCell'
+        | 'hide'
+        | 'icon'
+
+    MUIDataTableToolbar:
+        | 'root'
+        | '@media screen and (max-width: 480px)'
+        | "[theme.breakpoints.down('sm')]"
+        | "[theme.breakpoints.down('xs')]"
+        | 'actions'
+        | 'filterCloseIcon'
+        | 'filterPaper'
+        | 'fullWidthActions'
+        | 'fullWidthLeft'
+        | 'fullWidthRoot'
+        | 'fullWidthTitleText'
+        | 'icon'
+        | 'iconActive'
+        | 'left'
+        | 'searchIcon'
+        | 'titleRoot'
+        | 'titleText'
+
+    MUIDataTableToolbarSelect: 'root' | 'deleteIcon' | 'iconButton' | 'title'
+
+    MUIDataTableViewCol:
+        | 'root'
+        | 'checkbox'
+        | 'checkboxRoot'
+        | 'checked'
+        | 'formControl'
+        | 'formGroup'
+        | 'label'
+        | 'title'
   }
 }
