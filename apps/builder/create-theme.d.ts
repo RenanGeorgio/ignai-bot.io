7e1a367161fb8a76485bd8dc8bc0471694ddc123
RenanGeorgio <regeo9@hotmail.com>
2024-05-22 04:27:14 -0300

    try

--- apps/builder/create-theme.d.ts
+++ apps/builder/create-theme.d.ts
@@ -1,8 +1,24 @@
+import '@mui/material/styles'
+
 declare module '@mui/material/styles' {
   interface Theme {
     status: {
       danger: string
     }
+  }
+
+  interface Palette {
+    neutral: Palette["primary"];
+  }
+  interface PaletteOptions {
+    neutral: PaletteOptions["primary"];
+  }
+
+  interface PaletteColor {
+    darker?: string;
+  }
+  interface SimplePaletteColorOptions {
+    darker?: string;
   }
   // allow configuration using `createTheme`
   interface ThemeOptions {
