43505d4071c79981f0443d63c808e8d27bf72bb8
RenanGeorgio <43246444+RenanGeorgio@users.noreply.github.com>
2024-05-23 20:04:36 -0300

    Update LiteBadge.tsx

--- packages/embeds/js/src/components/LiteBadge.tsx
+++ packages/embeds/js/src/components/LiteBadge.tsx
@@ -47,7 +47,7 @@ export const LiteBadge = (props: Props) => {
       id="lite-badge"
     >
       <TypebotLogo />
-      <span>Made with Ignai</span>
+      <span>Made with Ignai-bot</span>
     </a>
   )
 }
