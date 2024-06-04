b70305c26cb56a24ff860b21309a53ee3f57e4f8
RenanGeorgio <regeo9@hotmail.com>
2024-06-04 15:24:55 -0300

    test

--- apps/builder/src/components/SideBar.tsx
+++ apps/builder/src/components/SideBar.tsx
@@ -1,159 +1,62 @@
-import React from 'react';
 import {
-  IconButton,
   Box,
-  CloseButton,
-  Flex,
-  Icon,
-  useColorModeValue,
-  Link,
+  Button,
   Drawer,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
   DrawerContent,
-  Text,
-  useDisclosure,
-  BoxProps,
-  FlexProps,
-} from '@chakra-ui/react';
-import { ReactText } from 'react';
-import { LaptopIcon } from './icons';
+  VStack,
+} from '@chakra-ui/react'
 
-interface LinkItemProps {
-  name: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  icon: any;
+interface Props {
+  onClose: Function
+  isOpen: boolean
+  variant: 'drawer' | 'sidebar'
 }
 
-interface MobileProps extends FlexProps {
-  onOpen: () => void;
-}
 
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
-
-interface NavItemProps extends FlexProps {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  icon: any;
-  children: ReactText;
-}
-
-const LinkItems: Array<LinkItemProps> = [
-  { name: 'Home', icon: LaptopIcon },
-  { name: 'Trending', icon: LaptopIcon },
-  { name: 'Explore', icon: LaptopIcon },
-  { name: 'Favourites', icon: LaptopIcon },
-  { name: 'Settings', icon: LaptopIcon },
-];
-
-export default function CustomSideBar({ children }: { children: React.ReactNode }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent
-        onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
-      />
-      <Drawer
-        autoFocus={false}
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full">
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        </DrawerContent>
-      </Drawer>
-      {/* mobilenav */}
-      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4">
-        {children}
-      </Box>
-    </Box>
-  );
-}
-
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
-  return (
+const CustomSideBar = ({ isOpen, variant, onClose }: Props) => {
+  return variant === 'sidebar' ? (
     <Box
-      transition="3s ease"
-      bg={useColorModeValue('white', 'gray.900')}
-      borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}>
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
-      </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
+      position="fixed"
+      left={0}
+      p={5}
+      w="200px"
+      top={0}
+      h="100%"
+      bg="#dfdfdf"
+    >
+      <SidebarContent onClick={onClose} />
     </Box>
-  );
-};
+  ) : (
+    <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Chakra-UI</DrawerHeader>
+          <DrawerBody>
+            <SidebarContent onClick={onClose} />
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  )
+}
 
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
-  return (
-    <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: 'cyan.400',
-          color: 'white',
-        }}
-        {...rest}>
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: 'white',
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
-    </Link>
-  );
-};
+const SidebarContent = ({ onClick }: { onClick: Function }) => (
+  <VStack>
+    <Button onClick={onClick} w="100%">
+      Home
+    </Button>
+    <Button onClick={onClick} w="100%">
+      About
+    </Button>
+    <Button onClick={onClick} w="100%">
+      Contact
+    </Button>
+  </VStack>
+)
 
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 24 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent="flex-start"
-      {...rest}>
-      <IconButton
-        variant="outline"
-        onClick={onOpen}
-        aria-label="open menu"
-        icon={<LaptopIcon />}
-      />
-
-      <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-        Logo
-      </Text>
-    </Flex>
-  );
-};
+export default CustomSideBar
--- apps/builder/src/features/dashboard/components/DashboardHeader.tsx
+++ apps/builder/src/features/dashboard/components/DashboardHeader.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { HStack, Flex, Spacer } from '@chakra-ui/react'
-import { LaptopIcon } from '@/components/icons'
+import React, { useState } from 'react'
+import { HStack, Flex, Spacer, Box, useBreakpointValue, IconButton } from '@chakra-ui/react'
+import { ChevronRightIcon, LaptopIcon } from '@/components/icons'
 import { useUser } from '@/features/account/hooks/useUser'
 import Link from 'next/link'
 import { EmojiOrImageIcon } from '@/components/EmojiOrImageIcon'
@@ -8,14 +8,37 @@ import { useWorkspace } from '@/features/workspace/WorkspaceProvider'
 import { WorkspaceDropdown } from '@/features/workspace/components/WorkspaceDropdown'
 import CustomSideBar from '@/components/SideBar'
  
+interface Props {
+  onShowSidebar: Function
+  showSidebarButton?: boolean
+}
+
+const smVariant = { navigation: 'drawer', navigationButton: true }
+const mdVariant = { navigation: 'sidebar', navigationButton: false }
+
 export const DashboardHeader = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false)
+  const variants = useBreakpointValue({ base: smVariant, md: mdVariant })
+
+  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen)
+
   return (
-    <CustomSideBar>
-      <DashboardHeaderContent />
-    </CustomSideBar>
+    <>
+      <CustomSideBar
+        variant={variants?.navigation}
+        isOpen={isSidebarOpen}
+        onClose={toggleSidebar}
+      />
+      <Box ml={!variants?.navigationButton && 200}>
+        <DashboardHeaderContent
+          showSidebarButton={variants?.navigationButton}
+          onShowSidebar={toggleSidebar}
+        />
+      </Box>
+    </>
   );
 }
-const DashboardHeaderContent = () => {
+const DashboardHeaderContent = ({ showSidebarButton = true, onShowSidebar }: Props) => {
   const { user, logOut } = useUser();
   const { workspace, switchWorkspace, createWorkspace } = useWorkspace();
 
@@ -23,6 +46,16 @@ const DashboardHeaderContent = () => {
 
   return (
     <Flex minWidth="max-content" alignItems="center" w="full" borderBottomWidth="1px" justify="center">
+      <Box flex="1">
+        {showSidebarButton && (
+          <IconButton
+            icon={<ChevronRightIcon w={8} h={8} />}
+            colorScheme="blackAlpha"
+            variant="outline"
+            onClick={onShowSidebar}
+          />
+        )}
+      </Box>
       <Link href="/typebots" data-testid="typebot-logo">
         <EmojiOrImageIcon
           boxSize="30px"
