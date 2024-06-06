4afba4f824099c09664750a00bb7767694de1e74
RenanGeorgio <regeo9@hotmail.com>
2024-06-06 19:20:52 -0300

    try

--- apps/builder/src/features/dashboard/components/DashboardPage.tsx
+++ apps/builder/src/features/dashboard/components/DashboardPage.tsx
@@ -2,7 +2,7 @@ import { Seo } from '@/components/Seo'
 import { useUser } from '@/features/account/hooks/useUser'
 import { PreCheckoutModal, PreCheckoutModalProps } from '@/features/billing/components/PreCheckoutModal'
 import { useWorkspace } from '@/features/workspace/WorkspaceProvider'
-import { Stack, VStack, Spinner, Text } from '@chakra-ui/react'
+import { Stack, VStack, Spinner, Text, Flex } from '@chakra-ui/react'
 import { Plan } from '@typebot.io/prisma'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
@@ -62,7 +62,6 @@ export const DashboardPage = () => {
     <Stack minH="100vh">
       <Seo title={workspace?.name ?? t('dashboard.title')} />
       <DashboardHeader />
-      <CustomSideBar />
       {!workspace?.stripeId && (
         <ParentModalProvider>
           <PreCheckoutModal
@@ -80,7 +79,10 @@ export const DashboardPage = () => {
             <Spinner />
           </VStack>
         ) : (
-          <FolderContent folder={null} />
+          <Flex w="100%">
+            <CustomSideBar />
+            <FolderContent folder={null} />
+          </Flex>
         )}
       </TypebotDndProvider>
     </Stack>
