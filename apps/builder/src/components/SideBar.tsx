bd31facd5dc30cbd2ad4dad66bf480712f0fd09f
RenanGeorgio <regeo9@hotmail.com>
2024-06-06 18:50:25 -0300

    try

--- apps/builder/src/components/SideBar.tsx
+++ apps/builder/src/components/SideBar.tsx
@@ -8,7 +8,6 @@ import {
   MenuButton,
   MenuList,
   Link,
-  Icon,
   IconButton
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from './icons'
@@ -24,10 +23,10 @@ type CustomElement =
   | HTMLButtonElement
   | HTMLDivElement;
 
-interface Props {
+export interface Props {
   btn?: React.MutableRefObject<CustomElement> | React.RefObject<CustomElement>
-  onClose: () => void
-  isOpen: boolean
+  onClose?: () => void
+  isOpen?: boolean
   variant?: string | undefined
 }
 
@@ -41,7 +40,7 @@ interface NavItemProps {
 
 interface HoverProps {
   title: string 
-  icon: JSX.Element
+  icon: JSX.Element | undefined
   description: string | undefined
 }
 
@@ -77,107 +76,7 @@ const SidebarContent = ({ onClick }: { onClick: () => void }) => (
 
 export default CustomSideBar*/
 
-
-const CustomSideBar = ({ isOpen, onClose }: Props) => {
-  const [navSize, changeNavSize] = useState<string>("large");
-  return (
-    <Flex
-      pos="sticky"
-      left="5"
-      h="95vh"
-      marginTop="2.5vh"
-      boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-      borderRadius={navSize == "small" ? "15px" : "30px"}
-      w={navSize == "small" ? "75px" : "200px"}
-      flexDir="column"
-      justifyContent="space-between"
-    >
-      <Flex
-        p="5%"
-        flexDir="column"
-        w="100%"
-        alignItems={navSize == "small" ? "center" : "flex-start"}
-        as="nav"
-      >
-        <IconButton
-          background="none"
-          aria-label="Colapse Sidebar"
-          mt={5}
-          _hover={{ background: 'none' }}
-          icon={<ChevronRightIcon />}
-          onClick={() => {
-            if (!isOpen) {
-              changeNavSize("large")
-            } else {
-              changeNavSize("small")
-              onClose()
-            }
-          }}
-        />
-        <NavItem navSize={navSize} icon={<ChevronRightIcon />} title="Dashboard" description="This is the description for the dashboard." />
-        <NavItem navSize={navSize} icon={<ChevronRightIcon />} title="Calendar" active />
-        <NavItem navSize={navSize} icon={<ChevronRightIcon />} title="Clients" />
-      </Flex>
-
-      <Flex
-        p="5%"
-        flexDir="column"
-        w="100%"
-        alignItems={navSize == "small" ? "center" : "flex-start"}
-        mb={4}
-      >
-        <Divider display={navSize == "small" ? "none" : "flex"} />
-        <Flex mt={4} align="center">
-          <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
-            <Heading as="h3" size="sm">Sylwia Weller</Heading>
-            <Text color="gray">Admin</Text>
-          </Flex>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
-}
-
-const NavItem = ({ icon, title, description, active, navSize }: NavItemProps) => {
-  return (
-    <Flex
-      mt={30}
-      flexDir="column"
-      w="100%"
-      alignItems={navSize == "small" ? "center" : "flex-start"}
-    >
-      <Menu placement="right">
-        <Link
-          backgroundColor={active ? "#AEC8CA" : undefined}
-          p={3}
-          borderRadius={8}
-          _hover={{ textDecor: 'none', backgroundColor: "#AEC8CA" }}
-          w={navSize == "large" ? "100%" : "35%"} // VERIFICAR
-        >
-          <MenuButton w="100%">
-            <Flex>
-              <Icon 
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                as={icon as any} fontSize="xl" color={active ? "#82AAAD" : "gray.500"} 
-              />
-              <Text ml={5} display={navSize == "small" ? "none" : "flex"}>{title}</Text>
-            </Flex>
-          </MenuButton>
-        </Link>
-        <MenuList
-          py={0}
-          border="none"
-          w={200}
-          h={200}
-          ml={5}
-        >
-          <NavHoverBox title={title} icon={icon} description={description} />
-      </MenuList>
-    </Menu>
-  </Flex>
-  );
-}
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const NavHoverBox = ({ title, icon, description }: HoverProps) => {
   return (
     <>
@@ -202,14 +101,105 @@ const NavHoverBox = ({ title, icon, description }: HoverProps) => {
         color="#fff"
         textAlign="center"
       >
-        <Icon 
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          as={icon as any} fontSize="3xl" mb={4} 
-        />
         <Heading size="md" fontWeight="normal">{title}</Heading>
         <Text>{description}</Text>
       </Flex>
     </>
+  );
+}
+
+const NavItem = ({ icon, title, description, active, navSize }: NavItemProps) => {
+  return (
+    <Flex
+      mt={30}
+      flexDir="column"
+      w="100%"
+      alignItems={navSize == "small" ? "center" : "flex-start"}
+    >
+      <Menu placement="right">
+        <Link
+          backgroundColor={active ? "#AEC8CA" : undefined}
+          p={3}
+          borderRadius={8}
+          _hover={{ textDecor: 'none', backgroundColor: "#AEC8CA" }}
+          w={navSize == "large" ? "100%" : "35%"} // VERIFICAR
+        >
+          <MenuButton w="100%">
+            <Flex>
+              <Text ml={5} display={navSize == "small" ? "none" : "flex"}>{title}</Text>
+            </Flex>
+          </MenuButton>
+        </Link>
+        <MenuList
+          py={0}
+          border="none"
+          w={200}
+          h={200}
+          ml={5}
+        >
+          <NavHoverBox title={title} icon={icon} description={description} />
+      </MenuList>
+    </Menu>
+  </Flex>
+  );
+}
+
+const CustomSideBar = () => {
+  const [navSize, changeNavSize] = useState<string>("large");
+  return (
+    <Flex
+      pos="sticky"
+      left="5"
+      h="95vh"
+      marginTop="2.5vh"
+      boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+      borderRadius={navSize == "small" ? "15px" : "30px"}
+      w={navSize == "small" ? "75px" : "200px"}
+      flexDir="column"
+      justifyContent="space-between"
+    >
+      <Flex
+        p="5%"
+        flexDir="column"
+        w="100%"
+        alignItems={navSize == "small" ? "center" : "flex-start"}
+        as="nav"
+      >
+        <IconButton
+          background="none"
+          aria-label="Colapse"
+          mt={5}
+          _hover={{ background: 'none' }}
+          icon={<ChevronRightIcon />}
+          onClick={() => {
+            if (navSize == "small") {
+              changeNavSize("large")
+            } else {
+              changeNavSize("small")
+            }
+          }}
+        />
+        <NavItem navSize={navSize} icon={<ChevronRightIcon />} title="Dashboard" description="This is the description for the dashboard." />
+        <NavItem navSize={navSize} icon={<ChevronRightIcon />} title="Calendar" active />
+        <NavItem navSize={navSize} icon={<ChevronRightIcon />} title="Clients" />
+      </Flex>
+
+      <Flex
+        p="5%"
+        flexDir="column"
+        w="100%"
+        alignItems={navSize == "small" ? "center" : "flex-start"}
+        mb={4}
+      >
+        <Divider display={navSize == "small" ? "none" : "flex"} />
+        <Flex mt={4} align="center">
+          <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
+            <Heading as="h3" size="sm">Sylwia Weller</Heading>
+            <Text color="gray">Admin</Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
