59d0dc40c3c2267293d09716c81c7020ade20b09
RenanGeorgio <regeo9@hotmail.com>
2024-06-11 18:26:47 -0300

    test

--- apps/builder/src/components/SideBar.tsx
+++ apps/builder/src/components/SideBar.tsx
@@ -6,7 +6,6 @@ import {
   Heading,
   Menu,
   MenuButton,
-  MenuItem,
   Link,
   IconButton,
   IconProps,
@@ -131,7 +130,7 @@ const NavItem = ({ title, description, active, navSize, router }: NavItemProps) 
           _hover={{ textDecor: 'none', backgroundColor: "rgba(255, 0, 0, 0.9)", color: "#fff", '& svg': { color: '#fff' } }}
           w={navSize == "large" ? "100%" : "35%"}
         >
-          <MenuButton w="100%" as={MenuItem} onClick={() => alert("oi")}>
+          <MenuButton w="100%" onClick={() => alert("oi")}>
             <SidebarContent tab={title} navSize={navSize} navigate={router} color={active ? "rgba(255, 0, 0, 0.9)" : "gray.500"} />
           </MenuButton>
         </Link>
