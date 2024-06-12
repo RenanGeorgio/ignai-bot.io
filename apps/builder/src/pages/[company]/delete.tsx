9ef27f74b61de3fbb4bdb8ef52a049cdacc5cb12
RenanGeorgio <regeo9@hotmail.com>
2024-06-12 20:01:14 -0300

    fix

--- apps/builder/src/pages/[company]/delete.tsx
+++ apps/builder/src/pages/[company]/delete.tsx
@@ -1,54 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { GetServerSidePropsContext } from 'next'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
-import { useRouter } from 'next/router'
-import { Text, Container, Button } from '@chakra-ui/react'
-import { ErrorPage } from '@/components/ErrorPage'
-import SuccessResult from '@/components/result/SuccessResult'
-import { useWorkspace } from '@/features/workspace/WorkspaceProvider'
-import { useTypebots } from '@/features/dashboard/hooks/useTypebots'
-import { useToast } from '@/hooks/useToast'
-import WarningResult from '@/components/result/WarningResult'
+import React from 'react';
+import { GetServerSidePropsContext } from 'next';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { Container } from '@chakra-ui/react';
+import { ErrorPage } from '@/components/ErrorPage';
+import SuccessResult from '@/components/result/SuccessResult';
+import { useWorkspace } from '@/features/workspace/WorkspaceProvider';
+import { useTypebots } from '@/features/dashboard/hooks/useTypebots';
+import { useToast } from '@/hooks/useToast';
+import WarningResult from '@/components/result/WarningResult';
 
 interface Props {
   company: string
   code: string | number
-  children?: React.ReactNode
   success: boolean
+  children?: React.ReactNode
 }
 
-export default function Page(
-  props: Props
-): InferGetServerSidePropsType<typeof getServerSideProps> {
-  const router = useRouter()
-  // retornar um resultado
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [reload, setReload] = useState<any>(undefined)
-  const [companyName, setCompanyName] = useState<string | undefined>(undefined)
-  const { workspace } = useWorkspace()
-  const { showToast } = useToast()
+export default function Page(props: Props): InferGetServerSidePropsType<typeof getServerSideProps> {
+  const router = useRouter();
+  const { workspace } = useWorkspace();
 
-  const {
-    typebots,
-    isLoading: isTypebotLoading,
-    refetch: refetchTypebots,
-  } = useTypebots({
+  const { showToast } = useToast();
+
+  const { typebots, isLoading: isTypebotLoading } = useTypebots({
     workspaceId: workspace?.id,
     folderId: 'root',
     onError: (error) => {
       showToast({
         description: error.message,
       })
-    },
-  })
-
-  useEffect(() => {
-    if (router) {
-      setReload(router?.reload)
     }
-
-    setCompanyName(props?.company)
-  }, [])
+  });
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
@@ -63,7 +46,7 @@ export default function Page(
   if (!(typebots?.length > 0)) {
     return (
       <WarningResult title="Atenção" description="Dados não foram excluídos" />
-    )
+    );
   }
 
   return (
@@ -71,12 +54,10 @@ export default function Page(
       title="Sucesso"
       description="Dados foram removidos com sucesso"
     />
-  )
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
   const company = context.params?.company as string | undefined
   const code = context.query?.id
   // verificar se o company e o code são validos
