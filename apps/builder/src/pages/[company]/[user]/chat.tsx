cb39fdb4750a0690f66fb563f109f73b291130c4
RenanGeorgio <regeo9@hotmail.com>
2024-06-12 04:20:12 -0300

    test

--- apps/builder/src/pages/[company]/[user]/chat.tsx
+++ apps/builder/src/pages/[company]/[user]/chat.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { GetServerSidePropsContext } from 'next';
-import { getServerSession } from 'next-auth';
-import { getAuthOptions } from '@/pages/api/auth/[...nextauth]';
-import { isNotDefined } from '@typebot.io/lib';
-import { User } from '@typebot.io/prisma';
+import prisma, { User } from '@typebot.io/prisma';
 import { ChatProvider } from '@/contexts/chat/ChatContext';
 import ChatPage from '@/components/Chat';
 
 import type {} from '@mui/x-data-grid/themeAugmentation';
 
 export default function Page() {
+  //const { replace } = useRouter();
+  //const { workspace } = useWorkspace();
+
+  /*
+  useEffect(() => {
+    if (!workspace || workspace.isPastDue) {
+      return
+    }
+
+    replace('/typebots');
+  }, [replace, workspace]);*/
+
   return (
     <ChatProvider>
       <ChatPage />
@@ -18,25 +27,26 @@ export default function Page() {
 }
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-  const session = await getServerSession(
-    context.req,
-    context.res,
-    getAuthOptions({})
-  );
-
   const userId = context.query.user?.toString() as string;
-  const user = session?.user as User;
 
-  if ((isNotDefined(session?.user)) || (user?.id != userId)) {
+  if (!userId) {
     return {
       redirect: {
         permanent: false,
         destination: `/signin`
       }
     }
-  } else {
-    return {
-      props: {}
-    }
+  }
+
+  const user = await prisma.user.findFirst({
+    where: { id: userId }
+  }) as User;
+
+  if (!user) {
+    return
+  }
+
+  return {
+    props: {}
   }
 }
