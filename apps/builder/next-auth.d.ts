044ec42ce6bb9a57419954f1802b8143bfef5766
RenanGeorgio <regeo9@hotmail.com>
2024-06-26 20:47:06 -0300

    test

--- apps/builder/next-auth.d.ts
+++ apps/builder/next-auth.d.ts
@@ -1,13 +1,11 @@
-import NextAuth, { DefaultSession } from 'next-auth'
+import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
   interface User {
     jwt: string
   }
 
-  interface Session {
-    user: {
-      jwt: string
-    } & DefaultSession["user"]
+  interface Profile {
+    jwt: string
   }
 }
