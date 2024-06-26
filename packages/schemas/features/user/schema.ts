0d809ff3cb8e2a6730cd374c1ac7fef53a6ccd9a
RenanGeorgio <43246444+RenanGeorgio@users.noreply.github.com>
2024-06-26 16:33:29 -0300

    Update schema.ts

--- packages/schemas/features/user/schema.ts
+++ packages/schemas/features/user/schema.ts
@@ -18,7 +18,6 @@ export const userSchema = z.object({
   graphNavigation: z.nativeEnum(GraphNavigation),
   preferredAppAppearance: z.string().nullable(),
   displayedInAppNotifications: displayedInAppNotificationsSchema.nullable(),
-  jwt: z.string().nullable(),
 }) satisfies z.ZodType<PrismaUser>
 
 export type User = z.infer<typeof userSchema>
